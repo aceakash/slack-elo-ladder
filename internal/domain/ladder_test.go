5e55b2f4bd6ea7a80d9a106d75d0452d1f1ceff7
Akash Kurdekar <akashkurdekar@gmail.com>
2021-10-14 23:22:34 +0100

    test compiles but fails

--- internal/domain/ladder_test.go
+++ internal/domain/ladder_test.go
@@ -1,26 +1,61 @@
 package domain_test
 
-import "testing"
-import "github.com/matryer/is"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestLadder(t *testing.T) {
-	is := is.New(t)
 
 	t.Run("When no matches have been played, all players are at the starting score", func(t *testing.T) {
 		// Given two players are registered
 		registerUser := NewRegisterUser()
-		registerUser.Execute("bruce")
-		registerUser.Execute("diana")
+		playerCount := 2
+		err := registerUser.Execute("bruce")
+		assert.NoError(t, err)
+		err = registerUser.Execute("diana")
+		assert.NoError(t, err)
 
 		// And no matches have been played
 
 		// When the ladder is computed
+		startingScore := 2000
 		computeLadder := NewComputeLadder()
-		ladder := computeLadder.Execute()
+		ladder, err := computeLadder.Execute()
+		assert.NoError(t, err)
 
 		// Then each player is at the starting score
-		for _, player := range ladder {
-			is.Equal(player.Score, settings.StartingScore())
+		assert.Equal(t, playerCount, len(ladder), "unexpected number of players in the ladder")
+		for _, entry := range ladder {
+			assert.Equal(t, startingScore, entry.Score)
 		}
 	})
+}
+
+type ComputeLadder struct {
+
+}
+
+type Ladder []struct {
+	Score int
+}
+
+func (cl ComputeLadder) Execute() (Ladder, error) {
+	return Ladder{}, nil
+}
+
+func NewComputeLadder() ComputeLadder {
+	return ComputeLadder{}
+}
+
+type RegisterUser struct {
+
+}
+
+func (ru RegisterUser) Execute(userId string) error {
+	return nil
+}
+
+func NewRegisterUser() RegisterUser {
+	return RegisterUser{}
 }
